f2b87025f060983768096785c9c859103df14494
parshva-b <parshva.barbhaya@somaiya.edu>
2019-12-20 13:37:50 +0530

    6.17

--- part6/redux-anecdotes/src/App.js
+++ part6/redux-anecdotes/src/App.js
@@ -1,11 +1,20 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { connect } from 'react-redux'
 
 import AnecdoteForm from './components/AnecdoteForm'
 import AnecdoteList from './components/AnecdoteList'
 import Notification from './components/Notification'
 import Filter from './components/Filter'
 
-const App = () => {
+import anecdoteService from './services/anecdotes'
+import { initialization } from './reducers/anecdoteReducer'
+
+const App = (props) => {
+	useEffect( () => {
+		anecdoteService.getAll().then( notes => 
+			props.initialization( notes )
+		)
+	})
 
 	return (
 		<div>
@@ -19,4 +28,4 @@ const App = () => {
 	)
 }
 
-export default App
+export default connect( null, { initialization })(App)
--- part6/redux-anecdotes/src/components/AnecdoteForm.js
+++ part6/redux-anecdotes/src/components/AnecdoteForm.js
@@ -2,21 +2,23 @@ import React from 'react'
 import { connect } from 'react-redux'
 
 import { createAnecdote } from '../reducers/anecdoteReducer'
+import anecdoteService from '../services/anecdotes'
 
 const AddAnecdote = (props) => {
 
-	const addNew = ( event ) => {
+	const addNew = async ( event ) => {
 		event.preventDefault()
-		props.createAnecdote( event.target.anecdote.value )
-		// console.log('added')
-		event.target.anecdote.value = ''
+		const content = event.target.content.value
+		event.target.content.value = ''
+		const newStory = await anecdoteService.postNew( content )
+		props.createAnecdote( newStory )
 	}
 	
 	return (
 		<>
 			<h2>create new</h2>
 			<form onSubmit = { addNew } >
-				<div><input name = "anecdote" /></div>
+				<div><input name = "content" /></div>
 				<button type="submit" >create</button>
 			</form>
 		</>
